68ea6a9f0dbb973ee44d941a689bb706ae62b73e
Cleber Miranda Barbosa <cleber@gmail.com>
2019-11-26 23:33:14 -0300

    simple solution

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ var noun = ['jogger','racoon'];
 function domainGenenator(pronoun, adj, noun) {
 
     for(let i=0; i< pronoun.length; i++) {
-
         for(let j=0; j<adj.length; j++) {
             for(let k=0; k<noun.length; k++) {
                 console.log(pronoun[i]+adj[j]+noun[k]+".com")
             }
         }
     }
-
 
 }
 
